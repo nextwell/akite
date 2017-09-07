fa86d6ecf6a6b5eb77c8d82662f036e9d453677b
nextwell <just.nextwell@gmail.com>
2017-09-07 23:28:04 +0300

    a href scroll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(() => {
 	console.log("loaded");
-	
+	$("a").click(function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href');
+        var top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1100);
+
+    });
 })
